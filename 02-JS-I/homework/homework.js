0f66a608d3a88606a2b93d8272f598311dbba18e
romiigonzalezn29 <rominagonzalezn29@gmail.com>
2022-02-18 16:06:52 -0300

    finish

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -210,9 +210,10 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola"+ " " + nombre + "!"
-  
+  let saludar = "Hola ".concat(nombre + "!")
+  return saludar;
 }
+
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
@@ -249,13 +250,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra == "a" || letra == "e" || letra== "i" || letra == "o" || letra == "u" ) {
-    return "Es vocal";
-
-    } 
-  return "Dato incorrecto"
-
+  if (letra.length > 1)
+    return "Dato incoarrecto";
+  else
+    if (letra === "a"||letra === "e"||letra === "i"||letra === "o"||letra === "u" )
+      return "Es vocal";
+    else 
+      return "Dato incorrecto";
 }
+
 
 
 
